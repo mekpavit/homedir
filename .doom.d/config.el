0fe1b6dc937b7dc1be22e78a60eab7776ef83770
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2021-01-11 00:21:08 +0700

    fix lsp and add more utility for Go

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -7,45 +7,64 @@
 
 (setq display-line-numbers-type 'relative)
 
-(defun my/go-coverage-all ()
+(defun my/go-impl--collect-interface (package)
+  (with-temp-buffer
+    (unless (zerop (process-file "go" nil t nil "doc" "-src" package))
+      (error "Failed: 'go doc -src %s'" package))
+    (goto-char (point-min))
+    (cl-loop with re = "^type\\s-+\\(\\S-+\\)\\s-+interface"
+             with real-package = (go-impl--real-package-name package)
+             while (re-search-forward re nil t)
+             collect (concat real-package "." (match-string-no-properties 1)) into interfaces
+             finally return (progn
+                              (puthash package (cl-copy-list interfaces) go-impl--interface-cache)
+                              interfaces))))
+(advice-add 'go-impl--collect-interface
+            :override 'my/go-impl--collect-interface)
+
+(setq my/go-build-tags "")
+(defun my/go--set-build-tags (build-tags)
+    (interactive
+     (list (read-from-minibuffer "Go build tags: ")))
+    "Accept build tags (separated by commas), set lsp-gopls-build-flags to ['tags=build-tags] and restart language server"
+  (setq my/go-build-tags build-tags)
+  (setq lsp-go-build-flags (vector (concat "-tags=" my/go-build-tags)))
+  (dolist (workspace (lsp-workspaces)) (lsp-workspace-restart workspace)))
+
+(defun my/go--coverage-all ()
   "Show go coverage of current buffer."
   (interactive)
-  (shell-command "go test -tags=integration -coverprofile=cover.out ./...")
+  (shell-command (concat "go test -tags=" my/go-build-tags " -coverprofile=cover.out ./..."))
   (go-coverage "cover.out"))
 
-;; override Doom's function
-(defun my/go-test-single ()
-  (interactive)
-  (if (string-match "_test\\.go" buffer-file-name)
-      (save-excursion
-        (re-search-backward "^func[ ]+\\(([[:alnum:]]*?[ ]?[*]?[[:alnum:]]+)[ ]+\\)?\\(Test[[:alnum:]_]+\\)(.*)")
-        (+go--run-tests (concat "-tags=integration " "-run" "='" (match-string-no-properties 2) "'")))
-    (error "Must be in a _test.go file")))
-
-;; override Doom's function
-(defun my/go-test-all ()
-  (interactive)
-  (+go--run-tests "-tags=integration"))
+(defun my/+go--run-tests (args)
+  (let ((cmd (concat "go test " (concat "-tags=" my/go-build-tags " " args))))
+    (setq +go-test-last (concat "cd " default-directory ";" cmd))
+    (+go--spawn cmd)))
+(advice-add '+go--run-tests
+            :override 'my/+go--run-tests)
 
 (map! :map go-mode-map
       :localleader
+      (:prefix ("b" . "build")
+       "f" #'my/go--set-build-tags)
       (:prefix ("c" . "coverage")
-       "a" #'go-coverage-all
-       :prefix ("t" . "test")
-       "s" #'my/go-test-single
-       "a" #'my/go-test-all))
+       "a" #'my/go--coverage-all))
 
 (setq org-directory "~/org/gtd/")
-(setq org-agenda-files '("~/org/gtd/next_actions.org" "~/org/gtd/projects.org" "~/org/gtd/calendar.org"))
-(setq org-capture-templates
-       `(("i" "In" entry  (file+headline "~/org/gtd/in.org" "In")
-        ,(concat "* (Added on %U) %?\n"))))
+(setq org-agenda-files '("~/org/gtd/next_actions.org" "~/org/gtd/projects.org" "~/org/gtd/calendar.org" "~/org/gtd/waiting_for.org"))
+(after! org
+  (add-to-list 'org-capture-templates
+               '("i" "In" entry
+                  (file+headline "~/org/gtd/in.org" "In")
+                 "* TODO (Added on %U) %?\n")))
 (setq org-refile-targets '(("~/org/gtd/projects.org" :maxlevel . 2)
                            ("~/org/gtd/next_actions.org" :level . 1)
                            ("~/org/gtd/calendar.org" :level . 1)
                            ("~/org/gtd/wanting_for.org" :level . 1)
                            ("~/org/gtd/referrences.org" :level . 1)
-                           ("~/org/gtd/someday.org" :level . 1)))
-(add-hook! org-load
+                           ("~/org/gtd/someday.org" :level . 1)
+                           ("~/org/gtd/trash.org" :level . 1)))
+(after! org
         (setq org-todo-keywords
-              '((sequence "TODO" "DONE" "WAITING"))))
+              '((sequence "TODO" "DONE" "WAIT" "HOLD"))))
