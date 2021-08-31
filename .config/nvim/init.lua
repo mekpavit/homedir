00e835f96d1301713eee0d82775199735413c4c7
pkiatkraipob <pavit.kiatkraipob@agoda.com>
2021-08-31 09:34:30 +0700

    use treesitter only on scala

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -296,7 +296,7 @@ require('gitsigns').setup()
 
 -- START config treesitter
 require('nvim-treesitter.configs').setup({
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = {"scala"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
       enable = true,              -- false will disable the whole extension
     },
